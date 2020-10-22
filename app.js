dae900c20e290a0d44fa28f99b63cc22d7f6db0c
Hernan415 <hernan.hernandezmendoza@students.dominican.edu>
2020-10-22 15:19:16 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const Handlebars = require('handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+
+// Initialize express
+const express = require('express')
+const methodOverride = require('method-override')
+const app = express()
+
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
+
+// require handlebars
+const exphbs = require('express-handlebars');
+
+// override with POST having ?_method=DELETE or ?_method=PUT
+app.use(methodOverride('_method'))
+
+// Use "main" as our default layout
+app.engine('handlebars', exphbs({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
+// Use handlebars to render
+app.set('view engine', 'handlebars');
+
+
+// The following line must appear AFTER const app = express() and before your routes!
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const models = require('./db/models');
+
+require('./controllers/events')(app, models);
+
+require('./controllers/events')(app, models);
+require('./controllers/rsvps')(app, models);
+
+
+// OUR MOCK ARRAY OF PROJECTS
+var events = [
+  { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+  { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+  { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
+]
+// Choose a port to listen on
+const port = process.env.PORT || 3000;
+
+// Tell the app what port to listen on
+app.listen(port, () => {
+  console.log('App listening on port 3000!')
+})
